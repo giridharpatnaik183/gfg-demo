4f900af98f00d56a5b64856e89a03e6b6377099b
Giridhar Sai Patnaik <94623129+giridharpatnaik183@users.noreply.github.com>
2023-09-08 12:08:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to serevr.....'
-scp -r build/* ubuntu@72.31.12.106:/var/www/72.31.12.106/
+cp -r /home/ubuntu/gfg-demo/* /var/www/html/gfg-demo/public/
+
